ebe394bc844a74c96dd97781e3b34c7af2125b6a
TanThanhLe <tanthanhcr95@gmail.com>
2024-06-03 22:45:25 +0700

    Introduction to Python Abstract Classes

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world")
+from abc import ABC
+from abc import abstractmethod
+# Python Abstract Classes
+
+class AbstractClassName(ABC):
+  @abstractmethod
+  def abstract_method_name(self):
+    pass
+
+if __name__ == "__main__":
+  ## Introduction to Python Abstract Classes
