b6306d03fc6fcf3d59384580acb4b7df86f7c4f1
TanThanhLe <tanthanhcr95@gmail.com>
2024-06-03 22:59:47 +0700

    The main program

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from abc import ABC
 from abc import abstractmethod
+
+from employee.base_employee import BaseEmployee
+from employee.fulltime_employee import FulltimeEmployee
+from employee.hourly_employee import HourlyEmployee
+
+from payroll import Payroll
+
 # Python Abstract Classes
 
 class AbstractClassName(ABC):
@@ -9,3 +16,13 @@ class AbstractClassName(ABC):
 
 if __name__ == "__main__":
   ## Introduction to Python Abstract Classes
+
+  payroll = Payroll()
+
+  payroll.add(FulltimeEmployee("John", "Doe", 6000))
+  payroll.add(FulltimeEmployee("Jane", "Doe", 6500))
+  payroll.add(HourlyEmployee("Jenifer", "Smith", 200, 50))
+  payroll.add(HourlyEmployee("David", "Wilson", 150, 100))
+  payroll.add(HourlyEmployee("Kevin", "Miller", 100, 150))
+
+  payroll.print()
